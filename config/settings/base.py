dee906ce5b418d42e5c40caa5133159427d39ed8
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 00:45:01 +0900

    jwt blacklist 관련 app 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # library
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     # apps
     "apps.accounts",
     "apps.boards",

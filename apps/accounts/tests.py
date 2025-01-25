25d5c8f89bde52e22f6a30c16a004ba3f05e8a38
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 02:15:48 +0900

    유저 관련 로직(회원가입, 로그인, 로그아웃, refresh token 갱신) test code 구현

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,33 +1,174 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
+from apps.accounts.models.users import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.test import APIClient
 
 
-class PostTestCase(TestCase):
+class AccountTestCase(TestCase):
 
-    @classmethod
-    def setUp(cls) -> None:
-        cls.user_1 = User.objects.create(username="user_1")
+    def test_signup(self):
+        # 유저 정보
+        email = "test@test.com"
+        name = "testuser"
+        password = "testpassword"
+        wrong_password = "wrongtestpassword"
 
-    def test_댓글_목록을_조회한다(self):
-        # 애니메이션 댓글 생성
-        comment = Comment.objects.create(
-            user=self.user_1, animation=self.ani_1, content="댓글 내용"
+        # 회원가입
+        path = "/users/signup"
+        request_data = {
+            "email": email,
+            "name": name,
+            "password": password,
+            "password_check": password,
+        }
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
         )
-        reported_comment = Comment.objects.create(
-            user=self.user_2, animation=self.ani_1, content="댓글 내용"
-        )
-        # 댓글 신고 3회
-        comment_reports = [
-            CommentReport(comment=reported_comment, user=user, report_type="normal")
-            for user in User.objects.all()
-        ]
-        CommentReport.objects.bulk_create(comment_reports)
 
-        # 해당 애니메이션 댓글 목록 확인
-        path = f"/comments?animation-id={self.ani_1.id}"
-        response = self.client.get(path=path, content_type="application/json")
-
-        # 애니메이션과 연결된 댓글이 1개 존재한다. (신고수가 3개 이상인 댓글은 조회 목록에서 제외)
+        # 검증
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["id"], comment.id)
+        self.assertEqual(response.data["email"], email)
+        self.assertEqual(response.data["name"], name)
+        self.assertTrue(response.data["is_active"])
+
+        # 이미 가입된 이메일로 회원가입
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # 검증
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["code"], "ALREADY_SIGNED_UP_EMAIL")
+
+        # 비밀번호가 서로 다른경우 회원가입
+        request_data = {
+            "email": email,
+            "name": name,
+            "password": password,
+            "password_check": wrong_password,
+        }
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # 검증
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data["code"], "PASSWORD_NOT_MATCHED")
+
+    def test_login(self):
+        # 유저 정보
+        email = "test@test.com"
+        name = "testuser"
+        password = "testpassword"
+        wrong_password = "wrongtestpassword"
+
+        # 유저 생성
+        user = User(email=email, name=name)
+        user.set_password(password)
+        user.save()
+
+        # 로그인
+        path = "/users/login"
+        request_data = {"email": email, "password": password}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # access token -> user
+        jwt_authentication = JWTAuthentication()
+        validated_token = jwt_authentication.get_validated_token(
+            response.data["token"]["access"]
+        )
+        user_from_token = jwt_authentication.get_user(validated_token)
+
+        # 검증
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["user"]["id"], user.pk)
+        self.assertEqual(response.data["user"]["name"], user.name)
+        self.assertEqual(user_from_token.id, user.pk)
+
+        # email/password 틀린 경우 로그인
+        request_data = {"email": email, "password": wrong_password}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # 검증
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data["code"], "AUTHENTICATED_FAILED")
+
+    def test_refresh_token(self):
+        # 유저 정보
+        email = "test@test.com"
+        name = "testuser"
+        password = "testpassword"
+
+        # 유저 생성
+        user = User(email=email, name=name)
+        user.set_password(password)
+        user.save()
+
+        # refresh_token get
+        refresh = RefreshToken.for_user(user)
+
+        # refresh 토큰으로 access token 요청
+        path = "/users/refresh"
+        request_data = {"refresh": str(refresh)}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # access token -> user
+        jwt_authentication = JWTAuthentication()
+        validated_token = jwt_authentication.get_validated_token(
+            response.data["access"]
+        )
+        user_from_token = jwt_authentication.get_user(validated_token)
+
+        # 검증
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("access" in response.data)
+        self.assertEqual(user_from_token.pk, user.pk)
+
+    def test_logout(self):
+        # 유저 정보
+        email = "test@test.com"
+        name = "testuser"
+        password = "testpassword"
+
+        # 유저 생성
+        user = User(email=email, name=name)
+        user.set_password(password)
+        user.save()
+
+        # 로그인
+        path = "/users/login"
+        request_data = {"email": email, "password": password}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+        refresh_token = response.data["token"]["refresh"]
+
+        # 로그아웃
+        path = "/users/logout"
+        self.client.login(email=email, password=password)
+        request_data = {"refresh": refresh_token}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+        self.client.logout()
+
+        # 검증
+        self.assertEqual(response.status_code, 204)
+
+        # refresh token 무효화 검증을 위해 refresh 토큰으로 access token 요청
+        path = "/users/refresh"
+        request_data = {"refresh": refresh_token}
+        response = self.client.post(
+            path=path, data=request_data, content_type="application/json"
+        )
+
+        # 검증
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data["code"], "TOKEN_IS_INVALID_OR_EXPIRED")

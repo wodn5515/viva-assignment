ffe9a8fac90951694878f4563c85a794eada172e
Jaewoo Kil <wodn5515@gmail.com>
2025-01-25 17:19:42 +0900

    user 모델 name 필드 unique옵션 제거

--- apps/accounts/models/users.py
+++ apps/accounts/models/users.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
         max_length=255,
         unique=True,
     )
-    name = models.CharField(max_length=20, null=False, unique=True)
+    name = models.CharField(max_length=20, null=False)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)

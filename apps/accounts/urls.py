1b3030392f68eb3e81c2d486c5b38e098ea9164b
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 01:09:47 +0900

    token refresh view 구현

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from apps.accounts.views.users import SignUpAPIView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("users/signup", SignUpAPIView.as_view()),
     path("users/login", LoginView.as_view()),
-    # path("users/refresh"),
+    path("users/refresh", TokenRefreshView.as_view()),
     # path("users/logout"),
 ]

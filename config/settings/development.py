119f1653144ad198804a84d343e898534e028adf
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 00:44:13 +0900

    test 용 DB 세팅

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,5 +1,11 @@
 from .base import *
+import sys
+import os
 
 DEBUG = True
 
 INSTALLED_APPS += ["django_extensions"]
+
+if "test" in sys.argv:
+    DATABASES["default"]["USER"] = os.getenv("MYSQL_ROOT_USERNAME")
+    DATABASES["default"]["PASSWORD"] = os.getenv("MYSQL_ROOT_PASSWORD")

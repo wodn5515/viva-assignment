76eed5ec5c233c78ab7a7338bab134178615bfdb
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 02:16:08 +0900

    토큰 인증실패 에러코드 분기처리

--- apps/utils/exception_handlers.py
+++ apps/utils/exception_handlers.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from rest_framework import exceptions
 from apps.utils import exceptions as custom_exceptions
 from django.core.exceptions import PermissionDenied
+from rest_framework_simplejwt.exceptions import InvalidToken
 from rest_framework.views import set_rollback
 from rest_framework.response import Response
 
@@ -11,6 +12,8 @@ def custom_exception_handler(exc, context):
         exc = custom_exceptions.NotFound()
     elif isinstance(exc, PermissionDenied):
         exc = custom_exceptions.PermissionDenied()
+    elif isinstance(exc, InvalidToken):
+        exc = custom_exceptions.NotAuthenticated()
 
     if isinstance(exc, exceptions.APIException):
         headers = {}

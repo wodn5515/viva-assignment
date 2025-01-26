e36d255481c3debb36e36264cf8106d716e19478
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 17:56:46 +0900

    토큰 인증 에러 핸들링 추가

--- apps/utils/exception_handlers.py
+++ apps/utils/exception_handlers.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from rest_framework import exceptions
 from apps.utils import exceptions as custom_exceptions
 from django.core.exceptions import PermissionDenied
-from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
 from rest_framework.views import set_rollback
 from rest_framework.response import Response
 
@@ -12,7 +12,7 @@ def custom_exception_handler(exc, context):
         exc = custom_exceptions.NotFound()
     elif isinstance(exc, PermissionDenied):
         exc = custom_exceptions.PermissionDenied()
-    elif isinstance(exc, InvalidToken):
+    elif isinstance(exc, InvalidToken) or isinstance(exc, AuthenticationFailed):
         exc = custom_exceptions.NotAuthenticated()
 
     if isinstance(exc, exceptions.APIException):

976c57bb55bfa1fd44ec2980ec3124a71d3a723e
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 18:50:58 +0900

    post migrations 수정

--- apps/boards/models/posts.py
+++ apps/boards/models/posts.py
@@ -1,10 +1,11 @@
-from django.db import models
-from apps.common.models import TimeStampedModel
+from apps.common.models import DjongoTimeStampedModel
+from djongo import models
 
 
-class Post(TimeStampedModel):
+class Post(DjongoTimeStampedModel):
     title = models.CharField(max_length=256)
     content = models.TextField()
+    author = models.IntegerField(db_index=True)
 
     class Meta:
         db_table = "posts"
--- apps/common/models.py
+++ apps/common/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from djongo import models as djongo_models
 
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class DjongoTimeStampedModel(djongo_models.Model):
+    created_at = djongo_models.DateTimeField(auto_now_add=True)
+    updated_at = djongo_models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True

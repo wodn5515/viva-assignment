88e2a0e24b6287e5dd70e817c8e742a1ca4b1641
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 21:07:22 +0900

    BaseService 클래스에 get_queryset 메서드 추가

--- apps/common/services.py
+++ apps/common/services.py
@@ -10,3 +10,16 @@ class BaseService:
 
     def hard_delete_by_id(self, id: int):
         self.model.objects.filter(id=id).delete()
+
+    def get_queryset(
+        self, page: int | None, page_size: int, order_by: str | None, **filter
+    ):
+        queryset = self.models.objects.filter(**filter)
+        if order_by:
+            queryset = queryset.order_by(order_by)
+        start, end = 0, page_size
+        if page:
+            start = (page - 1) * page_size
+            end = page * page_size
+
+        return queryset[start:end]

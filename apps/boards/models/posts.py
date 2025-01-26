61db6c3f739b7454923051ff1c2d85f6d92e0034
Jaewoo Kil <wodn5515@gmail.com>
2025-01-26 23:52:52 +0900

    djongo 라이브러리 이슈로 is_deleted 필드 integerfield로 변경

--- apps/boards/models/posts.py
+++ apps/boards/models/posts.py
@@ -6,7 +6,7 @@ class Post(DjongoTimeStampedModel):
     title = models.CharField(max_length=256)
     content = models.TextField()
     author_id = models.IntegerField(db_index=True)
-    is_deleted = models.BooleanField(default=False)
+    is_deleted = models.IntegerField(default=0)
     deleted_at = models.DateTimeField(null=True)
 
     class Meta:
